b0c110249c77fd5acc6a4be15ab13322482400de
unknown <nguyenghi111@.gmail.com>
2020-05-15 05:41:18 +0700

    add

---
+++ FileIO/BaiTapFileIO/Helper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BaiTapFileIO
+{
+    class Helper
+    {
+        public static int SumMatrix(int [,] matrix)
+        {
+            int sum = 0;
+            for( int i= 0; i< matrix.GetLength(0); i++)
+            {
+                for( int j =0; j< matrix.GetLength(1); j++)
+                {
+                    sum = sum + matrix[i, j];
+                }
+            }
+            return sum;
+        }
+        public static int CountPrime(int[,] matrix)
+        {
+            int count = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (CheckPrime(matrix[i, j]))
+                        count++;
+                }
+            }
+            return count;
+        }
+        public static bool CheckPrime(int num)
+        {
+            if (num == 2)
+                return true;
+            if (num < 2)
+                return false;
+            for (int i = 3; i < num-1;i++)
+            { 
+                if( num %i ==0)
+                    return false;
+            }
+            return true;
+        }
+        public static bool CheckOddNumber(int num)
+        {
+            if (num % 2 != 0)
+                return true;
+            return false;
+        }
+        public static int CountOddnumber(int [,] matrix)
+        {
+            int count = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (CheckOddNumber(matrix[i,j]))
+                        count++;
+                }
+            }
+            return count;
+        }
+        public static int SumBoder(int [,] matrix)
+        {
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    {
+                        sum = sum + matrix[0, j]+ matrix[matrix.GetLength(0)-1,j]
+                            + matrix[i,0]+ matrix[i,matrix.GetLength(1)-1];
+                    }
+                }
+            }
+            return sum;
+        }
+        public static void ShowMatrixMul3( int[,] matrix,int mulnum)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    
+                        matrix[i,j]= matrix[i, j]*mulnum;
+                    
+                }
+            }
+        }
+    }
+}
---
+++ FileIO/BaiTapFileIO/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace BaiTapFileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathInput = @"G:\codegym\module2\FileIO\BaiTapFileIO\Files\InputData.txt";
+            string pathOutput = @"G:\codegym\module2\FileIO\BaiTapFileIO\Files\OutputData.txt";
+            int[,] matrix;
+            int row, col, sum, countprime, countoddnumber, sumboder;
+
+            using (StreamReader sr = File.OpenText(pathInput))
+            {
+                string line = "";
+                line = sr.ReadLine();
+                string[] rowcol = line.Split(" ");
+                row = int.Parse(rowcol[0]);
+                col = int.Parse(rowcol[1]);
+                matrix = new int[row, col];
+                int rowIndex = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] rows = line.Split(" ");
+                    for (int i = 0; i < rows.Length; i++)
+                    {
+                        matrix[rowIndex, i] = int.Parse(rows[i]);
+                    }
+                    rowIndex++;
+                }
+                sum = Helper.SumMatrix(matrix);
+                countprime = Helper.CountPrime(matrix);
+                countoddnumber = Helper.CountOddnumber(matrix);
+                sumboder = Helper.SumBoder(matrix);
+               // Helper.ShowMatrixMul3( matrix,3);
+            }
+
+            using (StreamWriter sw = File.CreateText(pathOutput))
+            {
+                sw.WriteLine($"{row} {col}");
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        sw.Write($"{matrix[i, j]}\t");
+                    }
+                    sw.WriteLine();
+                }
+                sw.WriteLine($"Tong cua ma tran: {sum}");
+                sw.WriteLine($"So nguyen to co trong ma tran:{countprime}");
+                sw.WriteLine($"So le cos trong ma tran: {countoddnumber}");
+                sw.WriteLine($"Tong duong vien {sumboder}");
+                sw.WriteLine($"{row},{col}");
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        sw.Write($"{matrix[i, j]*3}\t");
+                    }
+                    sw.WriteLine(" ");
+                }
+            }
+        }
+    
+    }
+}
